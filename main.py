75820578a8aff6bd5684286a051a45b35b66603b
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-02-19 00:02:16 +0300

    update bg, fg

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ root.title("MY: YT-load")
 # прозрачность окна
 root.attributes("-alpha", 0.5)
 # цвет фона
-root.config(bg='#D3D3D3')
+root.config(bg='#1f1f21')
 
 
 # механика кнопки Скачать
@@ -50,22 +50,22 @@ def Exit():
 
 
 # заголовок формы
-lb = Label(root, text="Загружаю видео с YouTube ", font=('Arial,15,bold'), bg='#D3D3D3')
+lb = ttk.Label(root, text="Загружаю видео с YouTube ", font=('Arial', 15), padding=8,
+               background="#1f1f21", foreground="#fc5100")
 lb.pack(pady=15)
 # пояснительный текст для поля с адресом
-lb1 = Label(root, text="Ссылка на видео :", font=('Arial,15,bold'), bg='#D3D3D3')
+lb1 = ttk.Label(root, text="Ссылка на видео:", font=('Arial', 15), background="#1f1f21", foreground="#fc5100")
 lb1.place(x=10, y=80)
 
 # поле ввода адреса видео
 link1 = StringVar()
-En1 = Entry(root, textvariable=link1, font=('Arial,15,bold'))
+En1 = Entry(root, textvariable=link1, font=('Arial', 12), background="#1f1f1f", foreground="#fc5100")
 En1.place(x=230, y=80)
 
 # кнопка скачивания
 btn_download = ttk.Button(root, text="Скачать", command=download)
 # расположение кнопки
 btn_download.pack(anchor="n", pady=70)
-
 
 # кнопки очистки и выхода
 btn_reset = ttk.Button(root, text="Очистить", command=reset)
