e091d5cdb59af7002d62d5e0dddfc3068d7bf177
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-02-22 13:06:17 +0300

    added font and stle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import pytube
 from tkinter import messagebox
+from tkinter import font
 
 # рисуем главное окно
 root = Tk()
@@ -15,6 +16,13 @@ root.title("MY: YT-load")
 root.attributes("-alpha", 0.5)
 # цвет фона
 root.config(bg='#1f1f21')
+font_all = font.Font(family="Arial", size=15, weight="normal", slant="roman")
+
+label_style = ttk.Style()
+label_style.configure("My.TLabel",  # имя стиля
+                      font="Arial 13",  # шрифт
+                      foreground="#fc5100",  # цвет текста
+                      background="#1f1f1f")  # фоновый цвет
 
 
 # механика кнопки Скачать
@@ -50,27 +58,27 @@ def Exit():
 
 
 # заголовок формы
-lb = ttk.Label(root, text="Загружаю видео с YouTube ", font=('Arial', 15), padding=8,
+lb = ttk.Label(root, text="Загружаю видео с YouTube ", font=font_all, padding=8,
                background="#1f1f21", foreground="#fc5100")
 lb.pack(pady=15)
 # пояснительный текст для поля с адресом
-lb1 = ttk.Label(root, text="Ссылка на видео:", font=('Arial', 15), background="#1f1f21", foreground="#fc5100")
+lb1 = ttk.Label(root, text="Ссылка на видео:", font=font_all, background="#1f1f21", foreground="#fc5100")
 lb1.place(x=10, y=80)
 
 # поле ввода адреса видео
 link1 = StringVar()
-En1 = Entry(root, textvariable=link1, font=('Arial', 12), background="#1f1f1f", foreground="#fc5100")
+En1 = Entry(root, textvariable=link1, font=font_all, background="#1f1f1f", foreground="#fc5100")
 En1.place(x=230, y=80)
 
 # кнопка скачивания
-btn_download = ttk.Button(root, text="Скачать", command=download)
+btn_download = ttk.Button(root, text="Скачать", command=download, style="My.TLabel")
 # расположение кнопки
 btn_download.pack(anchor="n", pady=70)
 
 # кнопки очистки и выхода
-btn_reset = ttk.Button(root, text="Очистить", command=reset)
+btn_reset = ttk.Button(root, text="Очистить", command=reset, style="My.TLabel")
 btn_reset.place(x=160, y=190)
-btn_exit = Button(root, text=" Выход ", command=Exit)
+btn_exit = ttk.Button(root, text=" Выход ", command=Exit, style="My.TLabel")
 btn_exit.place(x=250, y=190)
 
 # запускаем окно
